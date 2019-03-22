007e23894ae0eb8f2a7594d4374e70ac80e54542
trnapster <trnapster@protonmail.com>
2019-03-22 22:05:31 +0100

    02: add validators for horse speeds

--- src/main/java/at/ac/tuwien/sepm/assignment/individual/validator/impl/HorseValidator.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/individual/validator/impl/HorseValidator.java
@@ -4,22 +4,31 @@ import at.ac.tuwien.sepm.assignment.individual.entity.Horse;
 import at.ac.tuwien.sepm.assignment.individual.validator.IHorseValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class HorseValidator implements IHorseValidator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HorseValidator.class);
 
-    @Autowired
+    private static final int MINSPEED = 40;
+    private static final int MAXSPEED = 60;
+
     public HorseValidator() {
     }
 
     @Override
     public boolean validate(Horse horse) {
-      return this.hasValidName(horse);
+      return this.hasValidName(horse) && this.hasValidSpeed(horse);
     }
 
     private boolean hasValidName(Horse horse) {
       return !horse.getName().isEmpty() && horse.getName() != null;
+    }
+
+    private boolean hasValidSpeed(Horse horse) {
+        return horse.getMinSpeed() >= MINSPEED && horse.getMaxSpeed() >= MINSPEED
+            && horse.getMinSpeed() <= MAXSPEED && horse.getMaxSpeed() <= MAXSPEED
+            && horse.getMinSpeed() <= horse.getMaxSpeed();
     }
 }
