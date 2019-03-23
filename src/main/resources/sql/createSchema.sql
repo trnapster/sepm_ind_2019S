c635cb3fe94d43552b7010d946db084b46918d92
trnapster <trnapster@protonmail.com>
2019-03-23 16:00:50 +0100

    fill database with test Jockey data

--- src/main/resources/sql/createSchema.sql
+++ src/main/resources/sql/createSchema.sql
@@ -11,3 +11,14 @@ CREATE TABLE IF NOT EXISTS horse (
   updated   DATETIME     NOT NULL,
   obsolete  BOOLEAN      NOT NULL DEFAULT FALSE
 );
+
+-- create table jockey if not exists
+CREATE TABLE IF NOT EXISTS jockey (
+  id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+  public_id BIGINT AUTO_INCREMENT,
+  name      VARCHAR(255) NOT NULL,
+  skill     DOUBLE       NOT NULL,
+  created   DATETIME     NOT NULL,
+  updated   DATETIME     NOT NULL,
+  obsolete  BOOLEAN      NOT NULL DEFAULT FALSE
+);
